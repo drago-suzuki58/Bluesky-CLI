c7868c50d56f6b4387a909445f34fedb07d046a6
どらすず <dorasuzu58@gmail.com>
2024-03-04 03:23:46 +0900

    add function to save login information

--- module/login.py
+++ module/login.py
@@ -1,17 +1,31 @@
 from atproto import AsyncClient
+import configparser
 
 async def login():
     client = AsyncClient()
 
-    username = input("Your Username: ")
-    password = input("Your password: ")
-    try:
-        profile = await client.login(username, password)
-    except Exception as e:
-        print("Login failed: ")
-        return
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
 
-    print("Login successful!\n", profile.display_name)
+    if 'save' in config and config['save']['username'] != "username" and config['save']['password'] != "password":
+        username = config['save']['username']
+        password = config['save']['password']
+        try:
+            profile = await client.login(username, password)
+        except Exception as e:
+            print("Login failed: ")
+            return
+        print("Login successful!\n", profile.display_name)
+    else:
+        username = input("Your Username: ")
+        password = input("Your password: ")
+        try:
+            profile = await client.login(username, password)
+        except Exception as e:
+            print("Login failed: ")
+            return
+        print("Login successful!\n", profile.display_name)
+
     print("\033[38;5;33m" ,
         " ,////                            .//// ", "\033[32m", " _     _  _______  ___      _______  _______  __   __  _______    _______  _______ \n", "\033[38;5;33m"
         "//////////                    */////////", "\033[32m", "| | _ | ||       ||   |    |       ||       ||  |_|  ||       |  |       ||       |\n", "\033[38;5;33m"
@@ -27,3 +41,18 @@ async def login():
         "    .///////////////////////////////    ", "\033[32m", "|  _   | |   |___ |       ||    ___||_____  ||     |_ |_     _|\n", "\033[38;5;33m"
         "     //////////////  ,/////////////     ", "\033[32m", "| |_|   ||       ||       ||   |___  _____| ||    _  |  |   |  \n", "\033[38;5;33m"
         "        /////////      *////////*       ", "\033[32m", "|_______||_______||_______||_______||_______||___| |_|  |___|  \n", "\033[0m")
+
+    if config["save"]["save"] == "0":
+        while True:
+            save = input("Do you want to save your login info? (y/n): ")
+            if save == "y":
+                config = configparser.ConfigParser()
+                config['save'] = {'username': username, 'password': password, 'save': "1"}
+                with open('config/config.ini', 'w') as f:
+                    config.write(f)
+                print("Login info saved!")
+                break
+            elif save == "n":
+                break
+            else:
+                print("Invalid input. Please enter 'y' or 'n'.")
