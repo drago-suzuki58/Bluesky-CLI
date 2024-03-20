b9da53e4f5995ca9192075b07b510c4689077f77
どらすず <dorasuzu58@gmail.com>
2024-03-20 09:57:58 +0900

    update debug.py

--- scripts/post/debug.py
+++ scripts/post/debug.py
@@ -1,2 +1,4 @@
 def print_post(posts):
     print(posts)
+    with open('log/post_debug.log', 'w') as f:
+        f.write(posts)
--- scripts/profile/debug.py
+++ scripts/profile/debug.py
@@ -1,8 +1,14 @@
 def print_profile(profile):
     print(profile)
+    with open('log/profile_debug.log', 'w') as f:
+        f.write(profile)
 
 def print_followers(followers):
     print(followers)
+    with open('log/followers_debug.log', 'w') as f:
+        f.write(followers)
 
 def print_following(following):
     print(following)
+    with open('log/following_debug.log', 'w') as f:
+        f.write(following)
